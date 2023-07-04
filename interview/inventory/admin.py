43720e5ebcaecc46530ca540eda4114d5b3e4439
James Eubanks <jeubanks.github@gmail.com>
2023-07-03 20:31:45 -0400

    setting up custom pagination class

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rest_framework.pagination import LimitOffsetPagination
 
 # Register your models here.
+
+class InventoryPagination(LimitOffsetPagination):
+    page_size = 3
+    
